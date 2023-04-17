a9358c6d7bf125be239c2f54883625b798d612ba
Michael <mschneider247@gmail.com>
2023-04-17 11:46:06 -0600

    Basic html

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <ul>
+          <li>Michael</li>
+          <l1>level 1</l1>
+          <li>🍔</li>
+        </ul>
+        <h3>SwOlLoTtO</h3>
       </header>
+      <body>
+        <h5>achievements</h5>
+        <h5>nutritionWatch</h5>
+        <h5>hootchHound</h5>
+        <h5>bodyWorks</h5>
+      </body>
     </div>
   );
 }
