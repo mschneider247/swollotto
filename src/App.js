8088442cbdb0306960f5166fd190d377519b617d
Michael <mschneider247@gmail.com>
2023-04-17 20:44:36 -0600

    Allow Google auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { firebaseConfig } from './Configs/firebaseConfig';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,31 +16,49 @@ import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB4sx9MRK9TXpiYMlXALbDOHL3PWRTaVXw",
-  authDomain: "swollotto.firebaseapp.com",
-  projectId: "swollotto",
-  storageBucket: "swollotto.appspot.com",
-  messagingSenderId: "282391073397",
-  appId: "1:282391073397:web:24c356ad7ef672f79ade3c",
-  measurementId: "G-QJ55BEMJDC"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const auth = getAuth();
+const analytics = getAnalytics(app);
 const firestore = getFirestore();
 
-const Account = () => {
-  return (
-    <h4>Hey Michael!</h4>
+const Account = () => { 
+  return auth.currentUser && (
+    <>
+      <h4>Hey Michael!</h4>
+      <button onClick={() => auth.signOut()}>Sign Out</button>
+    </>
   )
 };
 
 const SignIn = () => {
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+  };
+
   return (
-    <h4>Sign in</h4>
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
   )
 };
 
---
+++ src/Configs/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB4sx9MRK9TXpiYMlXALbDOHL3PWRTaVXw",
+    authDomain: "swollotto.firebaseapp.com",
+    projectId: "swollotto",
+    storageBucket: "swollotto.appspot.com",
+    messagingSenderId: "282391073397",
+    appId: "1:282391073397:web:24c356ad7ef672f79ade3c",
+    measurementId: "G-QJ55BEMJDC"
+  };
