fc94f87b8cd904693f3655f6be5cf48a4ed6a97d
Michael <mschneider247@gmail.com>
2023-04-24 20:18:27 -0600

    SignIn component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import { Account } from './Components/Account';
+import { SignIn } from './Components/SignIn';
 
 import { firebaseConfig } from './Configs/firebaseConfig';
 import { getAnalytics } from "firebase/analytics";
@@ -27,34 +28,34 @@ async function getLevel() {
 
 
 
-const SignIn = () => {
-  const signInWithGoogle = () => {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // IdP data available using getAdditionalUserInfo(result)
-      // ...
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
-  };
+// const SignIn = () => {
+//   const signInWithGoogle = () => {
+//     const provider = new GoogleAuthProvider();
+//     signInWithPopup(auth, provider)
+//     .then((result) => {
+//       // This gives you a Google Access Token. You can use it to access the Google API.
+//       const credential = GoogleAuthProvider.credentialFromResult(result);
+//       const token = credential.accessToken;
+//       // The signed-in user info.
+//       const user = result.user;
+//       // IdP data available using getAdditionalUserInfo(result)
+//       // ...
+//     }).catch((error) => {
+//       // Handle Errors here.
+//       const errorCode = error.code;
+//       const errorMessage = error.message;
+//       // The email of the user's account used.
+//       const email = error.customData.email;
+//       // The AuthCredential type that was used.
+//       const credential = GoogleAuthProvider.credentialFromError(error);
+//       // ...
+//     });
+//   };
 
-  return (
-    <button onClick={signInWithGoogle}>Sign in with Google</button>
-  )
-};
+//   return (
+//     <button onClick={signInWithGoogle}>Sign in with Google</button>
+//   )
+// };
 
 const WalkAbout = () => {
    return (
