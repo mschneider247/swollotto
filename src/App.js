df4684cc6af88d0aa2976f9037ee3ed4bb5153e1
Michael <mschneider247@gmail.com>
2023-04-24 20:22:12 -0600

    WalkAbout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 //react-firebase-hooks
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 import { Account } from './Components/Account';
 import { SignIn } from './Components/SignIn';
+import { WalkAbout } from './Components/WalkAbout';
 
-import { firebaseConfig } from './Configs/firebaseConfig';
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
-import { applicationDefault, cert } from "firebase/app";
 
 // Initialize Firebase
 import { app, db } from './Configs/firebaseConfig';
@@ -25,48 +23,6 @@ console.log(level);
 async function getLevel() {
   await getDoc(doc(db, "achievements", "level")).then((doc) => {console.log(doc)});
 }
-
-
-
-// const SignIn = () => {
-//   const signInWithGoogle = () => {
-//     const provider = new GoogleAuthProvider();
-//     signInWithPopup(auth, provider)
-//     .then((result) => {
-//       // This gives you a Google Access Token. You can use it to access the Google API.
-//       const credential = GoogleAuthProvider.credentialFromResult(result);
-//       const token = credential.accessToken;
-//       // The signed-in user info.
-//       const user = result.user;
-//       // IdP data available using getAdditionalUserInfo(result)
-//       // ...
-//     }).catch((error) => {
-//       // Handle Errors here.
-//       const errorCode = error.code;
-//       const errorMessage = error.message;
-//       // The email of the user's account used.
-//       const email = error.customData.email;
-//       // The AuthCredential type that was used.
-//       const credential = GoogleAuthProvider.credentialFromError(error);
-//       // ...
-//     });
-//   };
-
-//   return (
-//     <button onClick={signInWithGoogle}>Sign in with Google</button>
-//   )
-// };
-
-const WalkAbout = () => {
-   return (
-        <section>
-            <h4>WalkAbout</h4>
-            <button>Add Step Count</button>
-
-        </section>
-    )
-}
-
 
 function App() {
 
