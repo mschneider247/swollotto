73f9133dfd1cbd882f34584ccb75917d34596f03
Michael <mschneider247@gmail.com>
2023-04-26 20:29:28 -0600

    Clean

--- src/Components/Account.jsx
+++ src/Components/Account.jsx
@@ -3,17 +3,10 @@ import { getAuth } from "firebase/auth";
 export const Account = (props) => { 
     const auth = getAuth();
 
-    const signOutClearUser = async () => {
-        console.log('SIGN OUT')
-        await props.setUser()
-        await props.setLoggedIn(false)
-        auth.signOut()
-    }
-
     return auth.currentUser && (
       <>
         <p>Authenticated with Google</p>
-        <button onClick={() => signOutClearUser()}>Sign Out</button>
+        <button onClick={() => auth.signOut()}>Sign Out</button>
       </>
     )
   };
